936e76d53823129eeb08f99804c80b2b00a918fc
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2022-02-20 10:59:47 +0530

    Create Remove Covered Intervals.cpp

---
+++ Remove Covered Intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isCovered(int a, int b, int c, int d){
+        //check whether [a,b) is covered by [c,d)
+        return c <= a && b <= d;
+    };
+    
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int covered = 0;
+        int N = intervals.size();
+        
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(i != j && isCovered(intervals[i][0], intervals[i][1], intervals[j][0], intervals[j][1])) covered++;
+            }
+        }
+
+        return N-covered;
+    }
+};
